1e4dfa7729359b06c7bff21b2e3ded860f24f7b7
concatskills <34415032+concatskills@users.noreply.github.com>
2018-02-04 14:56:05 +0100

    Initial commit

---
+++ ProcessMonitor.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [Parameter(Mandatory=$True)] [int]$DurationInSec
+    )
+
+clear-host
+
+$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
+$ScriptDirectoryExe = "$($ScriptDirectory)\exe"
+$ScriptDirectoryOut = "$($ScriptDirectory)\out"
+
+Push-Location $ScriptDirectory
+
+$ProcMonExe = "$($ScriptDirectoryExe)\Procmon.exe"
+$CapturePml = "$($ScriptDirectoryOut)\capture.pml"
+$CaptureCsv = "$($ScriptDirectoryOut)\capture.csv"
+
+If(!(test-path $ScriptDirectoryOut)) { New-Item -ItemType Directory -Force -Path $ScriptDirectoryOut } else { Get-ChildItem -Path $ScriptDirectoryOut -Include *.* -Recurse  | remove-Item -Recurse -Force }
+ 
+# Warning UAC & run than admin
+Start-Process -FilePath $ProcMonExe -ArgumentList “/Quiet /AcceptEula /Minimized /Backingfile $CapturePml /Runtime $DurationInSec” -wait -Verb RunAs
+Start-Process -FilePath $ProcMonExe -ArgumentList "/Openlog $CapturePml /SaveAs $CaptureCsv" -wait -Verb RunAs
